afb6f405bc82ad923c2c1544ee5efbdbdbac00b3
Jeff <lijingzhi01@gmail.com>
2016-10-28 20:23:11 -0400

    R Programming_Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -2,15 +2,59 @@
 ## functions do
 
 ## Write a short comment describing this function 
-## test test2 test in local 
 
+
+# define "make matrix" function with matrix x
 makeCacheMatrix <- function(x = matrix()) {
 
+# set inverseMatrix(null firstly) to save the inverse values         
+        inverseMatrix <- NULL
+
+# set matrix function         
+        set <- function(y) {
+                x  <<- y
+                inverseMatrix  <<- NULL
+        }
+
+# get matrix function        
+        get <- function() x
+        
+# set inverse matrix fuction 
+        setinverseMatrix <- function(inverse)  inverseMatrix <<- inverse
+        
+# get inverse matrix function 
+        getinverseMatrix <- function() inverseMatrix
+
+# return a list of all four sub-functions        
+        list(set = set,
+             get = get,
+             setinverseMatrix = setinverseMatrix ,
+             getinverseMatrix = getinverseMatrix )
 }
 
-
-## Write a short comment describing this function
-
+###################################################
+# input the cached matrix 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        
+# find inverse matrix from cached matrix
+        inverseMatrix <- x$getinverseMatrix()
+
+# check if the inverse matrix we already have 
+# if have, return that inverse matrix
+        if(!is.null(inverseMatrix)) {
+                message("getting cached data")
+                return(inverseMatrix)
+        }
+
+# if not, get the function from x  
+        data <- x$get()
+        
+# get inverse matrix 
+        inverseMatrix <- solve(data, ...)
+
+# set/save the inverse matrix for further use      
+        x$setinverseMatrix(inverseMatrix )
+        
+# return results
+        inverseMatrix
 }
